c5ef19e8fc19eaac6b217ac8f3f6bd4dfa6827e4
nitishsghh <singhnitish627@outlook.com>
2023-07-01 00:50:06 +0530

    practice purpose only

---
+++ app.js
@@ -0,0 +1,13 @@
+let img = document.querySelector(".imgContainer img")
+function getPurple() {
+    img.setAttribute("src", "./images/1.jpg")
+}
+function getGold() {
+    img.setAttribute("src", "./images/2.jpg")
+}
+function getSilver() {
+    img.setAttribute("src", "./images/3.jpg")
+}
+function getBlack() {
+    img.setAttribute("src", "./images/4.jpg")
+}
